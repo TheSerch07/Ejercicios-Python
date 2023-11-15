2cb7c743c1f51cba1b715895f6983219cb5c0aee
TheSerch07 <jejog50@gmail.com>
2023-11-15 16:30:01 -0500

    second commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# number = int(input("Escribe un número:"))
+
+# if (number % 2 == 0):
+#     print("Me diste un número par!")
+# else:
+#     print("Me diste un número impar!")
+
+text = 'Equis de'
+
+print(text[::-1])
