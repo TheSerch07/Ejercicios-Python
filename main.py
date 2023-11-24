0c8612042642e69687374f48ab51d625976a32c1
TheSerch07 <jejog50@gmail.com>
2023-11-23 23:57:00 -0500

    add if

--- main.py
+++ main.py
@@ -5,15 +5,9 @@ options = ('piedra', 'papel', 'tijera')
 user_option = input("Piedra, papel o tijera?:").lower()
 computer_option = random.choice(options)
 
+if not user_option in options:
+    print('Tu opción no es valida')
+
+
 print(user_option)
 print(computer_option)
-# if (number % 2 == 0):
-#     print("Me diste un número par!")
-# else:
-#     print("Me diste un número impar!")
-
-# text = 'Equis de'
-
-# print(text[::-1])
-# print(text[2:5])
-# print(text[:-1])
